063d7fe3a2ca038b88229b9951bb1543cffa0f2b
JackTiew <teckchuan.tiew@simpletruss.com>
2025-01-02 14:38:00 +0800

    Change content of page.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import logo from "./logo.svg";
+import { useEffect } from "react";
 import "./App.css";
 import useVersionCheck from "./hooks/useVersionCheck";
 
 const App = () => {
   useVersionCheck();
 
-  const handleClick = () => {
-    alert("test");
-    window.location.reload();
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React9
-        </a>
-        <button onClick={handleClick}>test</button>
+        <img
+          src={`https://picsum.photos/id/${
+            Math.floor(Math.random() * (999 - 100 + 1)) + 100
+          }/600/600`}
+        />
       </header>
     </div>
   );
