64c5e2152332189bf350213325b6a2b76234aaa4
JackTiew <teckchuan.tiew@simpletruss.com>
2025-01-13 10:48:12 +0800

    1048.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={`https://picsum.photos/id/${750}/600/600`} />
+        <img src={`https://picsum.photos/id/${700}/600/600`} />
       </header>
     </div>
   );
